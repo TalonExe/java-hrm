d7a81b4a046a22eab6a7467c5be818e9cb9e9cd6
TalonExe <110237665+TalonExe@users.noreply.github.com>
2024-07-17 10:01:40 +0800

    added navigation functions

--- src/main/java/com/talon/views/employee/ApplyLeave.java
+++ src/main/java/com/talon/views/employee/ApplyLeave.java
@@ -11,4 +11,19 @@ public class ApplyLeave {
     private void backToApplyPage() throws IOException {
         App.setRoot("MainLobbyEmployee");
     }
+
+    @FXML
+    private void switchApplyLeave() throws IOException {
+        App.setRoot("ApplyLeave");
+    }
+
+    @FXML
+    private void switchPayroll() throws IOException {
+        App.setRoot("EmployeeSalary");
+    }
+
+    @FXML
+    private void switchProfile() throws IOException {
+        App.setRoot("EmployeeProfile");
+    }
 }
